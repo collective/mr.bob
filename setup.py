0e31a38ab9be8f70e98c88c677c7cd7a5a2d1cdb
Domen Kožar <domen@dev.si>
2013-01-23 14:20:56 +0100

    Back to development: 0.1a8

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def read(*rnames):
 
 
 setup(name='mr.bob',
-      version='0.1a7',
+      version='0.1a8.dev0',
       description='Bob renders directory structure templates',
       long_description=read('README.rst') + "\n" + read('HISTORY.rst'),
       classifiers=[
