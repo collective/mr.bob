d42723e872fc8dc005e16968e474bbcd1bfa5780
Gilles Lenfant <gilles.lenfant@gmail.com>
2013-02-11 01:33:58 +0100

    New style registration with a simple function

--- mrbob/__init__.py
+++ mrbob/__init__.py
@@ -1,42 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import os
-import abc
 import textwrap
+import sys
 
 import parsing
 
 
-class TemplateDescription(object):
-    """Base class for templates registered through setuptools entry_points
+class Attributes(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+def register_template(template_rel_dir, description=""):
+    """Register a project template
+
+    :param template_rel_dir: relative path to the template root (contains .mrbob.ini)
+    :param description: a full text description of the project template
     """
-    __metaclass__ = abc.ABCMeta
+    caller_file = sys._getframe(1).f_globals['__file__']  # The module that called register_template
+    template_dir = os.path.join(os.path.dirname(caller_file), template_rel_dir)
+    if not os.path.isdir(template_dir):
+        raise ValueError("{0} is not a directory".format(template_dir))
+    mrbob_ini_path = os.path.join(template_dir, '.mrbob.ini')
+    if not os.path.isfile(mrbob_ini_path):
+        raise ValueError("{0} is not a mr.bob template (has no .mrbob.ini file)".format(template_dir))
 
-    @abc.abstractproperty
-    def path(self):
-        """Absolute path to the template (folder that contains.mrbob.ini)
-        This property **must** be overriden by subclasses
-        """
-        return '/home/me/mytemplate'
-
-    @property
-    def description(self):
-        """Provides an unformatted description with useful end-user information.
-        By default, we shall provide the text that's in "description" option of
-        the "template" section of file .mrbob.ini
-        Anyway, authors of mrbob templates may override it
-        """
-        config = parsing.parse_config(os.path.join(self.path, '.mrbob.ini'))
+    if len(description) == 0:
+        # Try to get description from .mrbob.ini
+        config = parsing.parse_config(mrbob_ini_path)
         description = config.get('template', {}).get('description')
-        if description is not None:
-            return description
-        else:
-            return "No description available"
-
-    @property
-    def formatted_description(self):
-        """An UI suited description
-        """
-        indent = '    '
-        return textwrap.fill(self.description.strip(), width=79, initial_indent=indent,
-                             subsequent_indent=indent)
+        if description is None:
+            description = "No description available"
+    indent = '    '
+    description = textwrap.fill(description.strip(), width=79, initial_indent=indent,
+                                subsequent_indent=indent)
+    return Attributes(directory=template_dir, description=description)
